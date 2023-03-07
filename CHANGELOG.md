4312ee7421eea7feee4ded378d068b81a3dd7b7a
Piotr Mitkowski <piotr.mitkowski1@gmail.com>
2023-03-07 14:20:29 +0100

    API docs (#5)
    * Added the documentation for public APIs
    
    * Added the documentation for public APIs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.2
+* Added the API documentation
+
 ## 0.1.1
 * Fixed code format and license declaration 
 
--- lib/src/ar_quido_platform.dart
+++ lib/src/ar_quido_platform.dart
@@ -28,40 +28,53 @@ abstract class ARQuidoPlatform extends PlatformInterface {
     throw UnimplementedError('init() has not been implemented.');
   }
 
+  /// Sets the current state of the device's flashlight.
+  ///
+  /// The returned [Future] completes after the switch has been triggered on the
+  /// platform side.
   Future<void> toggleFlashlight({required bool shouldTurnOn}) {
     throw UnimplementedError('toggleFlashlight() has not been implemented.');
   }
 
+  /// Returns a stream that receives data when a reference image has been detected.
   Stream<ImageDetectedEvent> onImageDetected() {
     throw UnimplementedError('onImageDetected() has not been implemented.');
   }
 
+  /// Returns a stream that receives data when a reference image has been tapped.
   Stream<ImageTappedEvent> onDetectedImageTapped() {
     throw UnimplementedError('onImageDetected() has not been implemented.');
   }
 
+  /// Returns a stream that receives data when the recognition mode was started.
   Stream<RecognitionStartedEvent> onRecognitionStarted() {
     throw UnimplementedError(
       'onRecognitionStarted() has not been implemented.',
     );
   }
 
+  /// Returns a stream that receives data when the recognition mode was resumed
+  /// after a pause.
   Stream<RecognitionResumedEvent> onRecognitionResumed() {
     throw UnimplementedError(
       'onRecognitionResumed() has not been implemented.',
     );
   }
 
+  /// Returns a stream that receives data when the recognition mode was paused.
   Stream<RecognitionPausedEvent> onRecognitionPaused() {
     throw UnimplementedError(
       'onRecognitionResumed() has not been implemented.',
     );
   }
 
+  /// Returns a stream that receives data when the recognition module raises an
+  /// error.
   Stream<ErrorEvent> onError() {
     throw UnimplementedError('onError() has not been implemented.');
   }
 
+  /// Dispose of whatever resources the platform is holding on to.
   void dispose() {
     throw UnimplementedError('dispose() has not been implemented.');
   }
